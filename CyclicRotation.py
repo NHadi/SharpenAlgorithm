2f9524f7ba0213e317a45dcb70a60eec8dee41e4
Nurul Hadi <n.hadi1996@gmail.com>
2019-10-11 15:48:23 +0700

    factorial

--- CyclicRotation.py
+++ CyclicRotation.py
@@ -4,4 +4,4 @@ def solution(A, K):
     K = K % len(A)          
     return A[-K:] + A[:-K]
 
-print(solution([3, 8, 9, 7, 6], 3))    
+print(solution([3, 8, 9, 7, 6], 1))    
--- Polindrome.py
+++ Polindrome.py
@@ -2,7 +2,6 @@ def isPalindrome(n):
     return (n == n[::-1]) 
 
 n = 'hadi'
-print(n[:-1])
 if (isPalindrome(n)): 
     print("Yes") 
 else : 
